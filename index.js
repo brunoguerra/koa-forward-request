82d0e24422097bb31c9bd9e47f123ffdce5925d8
Bruno Guerra <brunoguerrait@gmail.com>
2016-05-16 15:58:44 -0300

    feat(koa2): using async

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var isUrl = require('is-url');
 module.exports = function forwardRequest(app, defaultOptions) {
   defaultOptions = defaultOptions || {};
 
-  app.context.forward = function (url, options) {
+  app.context.forward = function (ctx, url, options) {
     assert('string' === typeof url, 'first parameter must be a url string.');
 
     options = options || {};
@@ -20,21 +20,22 @@ module.exports = function forwardRequest(app, defaultOptions) {
       options.url = options.url || url;
     } else {
       options.url = options.url || url;
-      options.baseUrl = options.baseUrl || this.protocol + '://' + this.host;
+      options.baseUrl = options.baseUrl || ctx.protocol + '://' + ctx.host;
     }
-    options.method = options.method || this.method;
-    delete this.header.host;
-    options.headers = options.headers || this.header;
-    options.qs = options.qs || this.query;
+    options.method = options.method || ctx.method;
+    delete ctx.header.host;
+    options.headers = options.headers || ctx.header;
+    options.qs = options.qs || ctx.query;
 
-    switch (this.is('json', 'multipart/form-data', 'urlencoded')) {
+    switch (ctx.is('json', 'multipart/form-data', 'urlencoded')) {
     case 'json':
       delete options.headers['content-length'];
-      options.body = options.body || this.request.body;
+      options.body = options.body || ctx.request.body;
       options.json = true;
+      // options.headers['content-type'] = ctx.headers['content-type'];
       break;
     case 'multipart/form-data':
-      var body = this.request.body;
+      var body = ctx.request.body;
       var files = body.files || {};
       var fields = body.fields || {};
       if (!options.formData) {
@@ -56,15 +57,15 @@ module.exports = function forwardRequest(app, defaultOptions) {
       }
       break;
     case 'urlencoded':
-      options.form = options.form || this.request.body;
+      options.form = options.form || ctx.request.body;
       break;
     default:
       if (!~['HEAD', 'GET', 'DELETE'].indexOf(options.method)) {
-        options.body = options.body || this.request.body;
+        options.body = options.body || ctx.request.body;
       }
     }
     
-    var self = this;
+    var self = ctx;
     self.respond = false;
 
     if (options.debug) {
@@ -87,11 +88,24 @@ module.exports = function forwardRequest(app, defaultOptions) {
   module.exports.all = function all () {
     assert(defaultOptions.baseUrl, 'use `all()` must set `baseUrl` in options');
 
-    return function* (next) {
-      yield* next;
+    return handle; 
 
-      if (this.status === 404) {
-        this.forward(this.originalUrl);
+    function handle(ctx, next) {
+      const r = next();
+      if (r) {
+        r.then(check).catch((e) => ctx.throw(e));
+      } else {
+        check();
+      }
+      function check() {
+        if (ctx.status === 404) {
+          ctx.status = 200;
+          try {
+            ctx.forward(ctx, ctx.originalUrl);
+          } catch (e) {
+            ctx.throw(e);
+          }
+        }
       }
     };
   };
